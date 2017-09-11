8354a66375573df2a0302778481d619a3d2d47c0
Naor DM <naorcho@gmail.com>
2017-09-11 21:33:48 +0300

    Initial Commit

---
+++ app/src/main/java/com/appandgo/birthday/NDMainActivity.java
@@ -0,0 +1,13 @@
+package com.appandgo.birthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NDMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ndmain);
+    }
+}
