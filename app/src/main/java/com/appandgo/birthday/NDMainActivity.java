2a97e9a49c660c45d016d275e0df2db06687f9ec
Naor DM <naorcho@gmail.com>
2017-09-13 14:17:33 +0300

    -Added the Assets and acitivies.-Started the work on the second screen

--- app/src/main/java/com/appandgo/birthday/NDMainActivity.java
+++ app/src/main/java/com/appandgo/birthday/NDMainActivity.java
@@ -1,7 +1,9 @@
 package com.appandgo.birthday;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NDMainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class NDMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ndmain);
+    }
+
+    /** Called when the user clicks the next button */
+    public void btnNextPressed(View view) {
+
+        Intent i = new Intent(this, NDBirthdayActivity.class);
+        startActivity(i);
     }
 }
